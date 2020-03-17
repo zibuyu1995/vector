d1222e2645582b940af20cfd0accbefe4e37003a
binarylogic <bjohnson@binarylogic.com>
2020-03-17 18:42:35 -0400

    Switch s3 folder
    Signed-off-by: binarylogic <bjohnson@binarylogic.com>

--- scripts/release-s3.sh
+++ scripts/release-s3.sh
@@ -26,28 +26,28 @@ ls $td
 if [[ "$CHANNEL" == "nightly" ]]; then
   # Add nightly files with today's date for posterity
   today=$(date +"%F")
-  echo "Uploading all artifacts to s3://packages.timber.io/vector/nightly/$today"
-  aws s3 cp "$td" "s3://packages.timber.io/vector/nightly/$today" --recursive --sse --acl public-read
+  echo "Uploading all artifacts to s3://packages.timber.io/vector/tokio-compat/$today"
+  aws s3 cp "$td" "s3://packages.timber.io/vector/tokio-compat/$today" --recursive --sse --acl public-read
   echo "Uploaded archives"
 
   # Add "latest" nightly files
-  echo "Uploading all artifacts to s3://packages.timber.io/vector/nightly/latest"
-  aws s3 rm --recursive "s3://packages.timber.io/vector/nightly/latest"
-  aws s3 cp "$td" "s3://packages.timber.io/vector/nightly/latest" --recursive --sse --acl public-read
-  echo "Uploaded archives"
+  # echo "Uploading all artifacts to s3://packages.timber.io/vector/nightly/latest"
+  # aws s3 rm --recursive "s3://packages.timber.io/vector/nightly/latest"
+  # aws s3 cp "$td" "s3://packages.timber.io/vector/nightly/latest" --recursive --sse --acl public-read
+  # echo "Uploaded archives"
 
   # Set up redirects for historical locations
-  echo "Setting up redirects for historical locations"
-  aws s3api put-object \
-    --bucket packages.timber.io \
-    --key vector/nightly/latest/vector-x86_64-unknown-linux-gnu.tar.gz \
-    --website-redirect-location /vector/nightly/latest/vector-x86_64-unknown-linux-musl.tar.gz \
-    --acl public-read
+  # echo "Setting up redirects for historical locations"
+  # aws s3api put-object \
+  #   --bucket packages.timber.io \
+  #   --key vector/nightly/latest/vector-x86_64-unknown-linux-gnu.tar.gz \
+  #   --website-redirect-location /vector/nightly/latest/vector-x86_64-unknown-linux-musl.tar.gz \
+  #   --acl public-read
 
   # Verify that the files exist and can be downloaded
-  cmp <(curl https://packages.timber.io/vector/nightly/$today/vector-x86_64-unknown-linux-musl.tar.gz --fail) "$td/vector-x86_64-unknown-linux-musl.tar.gz"
-  cmp <(curl https://packages.timber.io/vector/nightly/latest/vector-x86_64-unknown-linux-musl.tar.gz --fail) "$td/vector-x86_64-unknown-linux-musl.tar.gz"
-  cmp <(curl -L https://packages.timber.io/vector/nightly/latest/vector-x86_64-unknown-linux-gnu.tar.gz --fail) "$td/vector-x86_64-unknown-linux-musl.tar.gz"
+  # cmp <(curl https://packages.timber.io/vector/nightly/$today/vector-x86_64-unknown-linux-musl.tar.gz --fail) "$td/vector-x86_64-unknown-linux-musl.tar.gz"
+  # cmp <(curl https://packages.timber.io/vector/nightly/latest/vector-x86_64-unknown-linux-musl.tar.gz --fail) "$td/vector-x86_64-unknown-linux-musl.tar.gz"
+  # cmp <(curl -L https://packages.timber.io/vector/nightly/latest/vector-x86_64-unknown-linux-gnu.tar.gz --fail) "$td/vector-x86_64-unknown-linux-musl.tar.gz"
 elif [[ "$CHANNEL" == "latest" ]]; then
   version_exact=$VERSION
   version_minor_x=$(echo $VERSION | sed 's/\.[0-9]*$/.X/g')
